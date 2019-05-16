ac07768135e7a34389b7f67ccd38a1afa35e83d8
Max Payne <forgge@gmail.com>
2019-05-16 14:47:45 +0300

    Fix empty telemetry bug

--- main.py
+++ main.py
@@ -34,22 +34,23 @@ class BtDeviceInterface:
 # Extracts data from MI sensor and sends it to TB
 class MiTempHumiditySensor(BtDeviceInterface):
     def __init__(self):
-        # Full-blown notifications are not supported: MI device will disconnect after a 
+        # Full-blown notifications are not supported: MI device will disconnect after a
         # few notifications sent.
         # So it is better to use poll mechanism instead
         BtDeviceInterface.__init__(self)
 
     def poll(self, bt_device):
-        telemetry = {}
         class MI_Delegate(DefaultDelegate):
             def __init__(self):
                 DefaultDelegate.__init__(self)
+                self.telemetry = {}
 
             def handleNotification(self, handle, data):
                 print("Received data:", data)
-                telemetry = { "temperature" : float(data[2:6]), "humidity" : float(data[9:13]) }
+                self.telemetry = { "temperature" : float(data[2:6]), "humidity" : float(data[9:13]) }
 
-        bt_device.withDelegate(MI_Delegate())
+        delegate = MI_Delegate()
+        bt_device.withDelegate(delegate)
 
         # This is a required part of MI sensor protocol.
         # Without it, notification will not be delivered.
@@ -57,7 +58,7 @@ class MiTempHumiditySensor(BtDeviceInterface):
         # UUID here. Instead a write operation is performed by handle.
         bt_device.writeCharacteristic(0x10, b'\x01\x00', True)
         bt_device.waitForNotifications(1)
-        return telemetry
+        return delegate.telemetry
 
 # Extracts data from ESP test device and sends it to TB. Both polling and notifications
 # are supported.
@@ -74,7 +75,7 @@ class EspGattDemoDevice(BtDeviceInterface):
         esp_char = esp_service.getCharacteristics("0000ff00-0000-1000-8000-00805f9b34fb")[0]
         char_value = str(esp_char.read(), 'utf-8')
 
-        return { "esp_char" : char_value }
+        return { "esp_char" : char_value.strip('\u0000') }
 
     def start_notify(self, bt_device):
         # No need in a special preparation before notification starts
@@ -99,7 +100,39 @@ class EspGattDemoDevice(BtDeviceInterface):
         print("Received GATT data from ESP:", data, "decoded:", decoded)
         return { "counter_noti": decoded }
 
-# Contains devices that we should connect to and extract data. 
+# Test device: arbitrary sensor
+class TestSensorDemoDevice(BtDeviceInterface):
+    def __init__(self):
+        BtDeviceInterface.__init__(self, noti_supported=True)
+        self.noti_started = False
+
+    def poll(self, bt_device):
+        return { }
+
+    def start_notify(self, bt_device):
+        # No need in a special preparation before notification starts
+        self.noti_started = True
+
+    def stop_notify(self):
+        # No need in a special preparation before notification starts
+        self.noti_started = False
+
+    def notify_started(self):
+        return self.noti_started
+
+    def handle_notify(self, handle, data):
+        # Helper routine
+        def bytes_to_int(bytes):
+            result = 0
+            for i in reversed(bytes):
+                result = (result << 8) + i
+            return result
+
+        decoded = bytes_to_int(data)
+        print("Received GATT data from Test sensor:", data, "decoded:", decoded)
+        return { "decoded_data": decoded }
+
+# Contains devices that we should connect to and extract data.
 # Some aux data is written in runtime
 known_devices = {
     "MJ_HT_V1": {
@@ -110,6 +143,13 @@ known_devices = {
         "handler": EspGattDemoDevice,
         "scanned": {}
     },
+    # TODO: add support for test sensors and random addresses
+    # (which require new scan every time)
+    # "TEST_SENSOR": {
+    #     "handler": TestSensorDemoDevice,
+    #     "scanned": {},
+    #     "addr_type": "random"
+    # }
 }
 
 #---------------------------------------------------------------------------------------------------
@@ -126,31 +166,36 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print("Received new data from:", dev.addr)
 
-print("Scanning BLE devices...")
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(10.0)
+known_devices_found = False
 
-for dev in devices:
-    print("Device {} ({}), RSSI={} dB".format(dev.addr, dev.addrType, dev.rssi))
-    for (adtype, desc, value) in dev.getScanData():
-        print("  {} = {}".format(desc, value))
-        if desc == "Complete Local Name" and value in known_devices:
-            print("    known device found:", value)
-            known_devices[value]["scanned"][dev.addr] = { 
-                "inst": known_devices[value]["handler"](),
-                "periph": Peripheral(),
-                "tb_name": value + "_" + dev.addr
-            }
+while not known_devices_found:
+    try:
+        print("Scanning BLE devices...")
+        scanner = Scanner().withDelegate(ScanDelegate())
+        devices = scanner.scan(15.0)
+
+        for dev in devices:
+            print("Device {} ({}), RSSI={} dB".format(dev.addr, dev.addrType, dev.rssi))
+            for (adtype, desc, value) in dev.getScanData():
+                print("  {} = {}".format(desc, value))
+                if desc == "Complete Local Name" and value in known_devices:
+                    print("    [!] Known device found:", value)
+                    known_devices[value]["scanned"][dev.addr] = {
+                        "inst": known_devices[value]["handler"](),
+                        "periph": Peripheral(),
+                        "tb_name": value + "_" + dev.addr
+                    }
+                    known_devices_found = True
+    except Exception as e:
+        print("Exception caught:", e)
 
 #---------------------------------------------------------------------------------------------------
 
-TB_SERVER = "demo.thingsboard.io"
-TB_ACCESS_TOKEN = "DmCbB394rtwuIwUMt8Bz"
+TB_SERVER = "localhost"
+TB_ACCESS_TOKEN = "ziVM6GV08dpGFOh1xBrp"
 
 gateway = TBGatewayMqttClient(TB_SERVER, TB_ACCESS_TOKEN)
 gateway.connect()
-
-# Starts notifications threads for each discovered device that support the notification mechanism
 
 while True:
     for type, type_data in known_devices.items():
@@ -158,12 +203,13 @@ while True:
             try:
                 instance = dev_data["inst"]
                 ble_periph = dev_data["periph"]
-                tb_dev_name = dev_data["tb_name"]
+                tb_dev_name = dev_data["tb_name"].replace(':', '')
 
                 telemetry = {}
 
                 print("Connecting to device:", tb_dev_name)
-                ble_periph.connect(dev_addr)
+
+                ble_periph.connect(dev_addr, "public")
 
                 if instance.notify_supported():
                     if instance.notify_started() == False:
@@ -178,14 +224,14 @@ while True:
                         def handleNotification(self, handle, data):
                             print("Received notifications for handle:", handle)
                             self.telemetry = self.dev_instance.handle_notify(handle, data)
-                            
+
                     print("Getting notification from:", tb_dev_name)
 
                     deleagate = NotiDelegate()
                     ble_periph.withDelegate(deleagate)
                     ble_periph.waitForNotifications(1)
                     print("Data received:", deleagate.telemetry)
-                    
+
                     telemetry.update(deleagate.telemetry)
 
                 print("Polling data from:", tb_dev_name)
@@ -194,15 +240,21 @@ while True:
 
                 telemetry.update(poll_telemetry)
 
-                gateway.gw_connect_device(tb_dev_name)
+                if not telemetry:
+                    print("No data to send for current device")
+                    continue
+
                 gateway_pkt = { "ts": int(round(time.time() * 1000)), "values" : telemetry }
+
                 print("Sending data to TB:", gateway_pkt)
+
+                gateway.gw_connect_device(tb_dev_name)
                 gateway.gw_send_telemetry(tb_dev_name, gateway_pkt)
                 gateway.gw_disconnect_device(tb_dev_name)
             except KeyboardInterrupt:
                 print("Exiting the application")
                 sys.exit()
-            except Exception as e: 
+            except Exception as e:
                 print("Exception caught:", e)
             finally:
                 print("Disconnecting from device")
